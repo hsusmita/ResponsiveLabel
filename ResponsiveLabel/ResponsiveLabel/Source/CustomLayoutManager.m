35931a4ba42f1f38b3ace8dfd463c42f939b47e3
hsusmita <susmita.horrow@gmail.com>
2016-01-08 17:09:35 +0530

    Custom background drawing only when needed

--- ResponsiveLabel/ResponsiveLabel/Source/CustomLayoutManager.m
+++ ResponsiveLabel/ResponsiveLabel/Source/CustomLayoutManager.m
@@ -12,37 +12,53 @@
 @implementation CustomLayoutManager
 
 - (void)drawBackgroundForGlyphRange:(NSRange)glyphsToShow atPoint:(CGPoint)origin {
-  NSRange range = glyphsToShow;
-  if (range.length == 0) {
-    return;
-  }
-  
-  NSTextContainer *textContainer = [self textContainerForGlyphAtIndex:range.location
-                                                       effectiveRange:nil];
-  CGContextRef context = UIGraphicsGetCurrentContext();
-  CGContextSaveGState(context);
-  [self.textStorage enumerateAttribute:NSBackgroundColorAttributeName
-                               inRange:range
-                               options:NSAttributedStringEnumerationLongestEffectiveRangeNotRequired
-                            usingBlock:^(id value, NSRange range, BOOL *stop) {
-    CGRect boundingRect = [self boundingRectForGlyphRange:range
-                                          inTextContainer:textContainer];
-    boundingRect.origin.x += origin.x;
-    boundingRect.origin.y += origin.y;
-    UIColor *backgroundColor = [self.textStorage attribute:NSBackgroundColorAttributeName
-                                                   atIndex:range.location
-                                            effectiveRange:nil];
-    CGFloat cornerRadius = ((NSNumber *)[self.textStorage attribute:RLHighlightedBackgroundCornerRadius
-                                                            atIndex:range.location
-                                                     effectiveRange:nil]).floatValue;
-    UIBezierPath *bezierPath = [UIBezierPath bezierPathWithRoundedRect:boundingRect
-                                                          cornerRadius:cornerRadius];
-    if (backgroundColor) {
-      CGContextSetFillColorWithColor(context, backgroundColor.CGColor);
-      [bezierPath fill];
-    }
-  }];
-  CGContextRestoreGState(context);
+	if (glyphsToShow.length == 0) {
+		return;
+	} else if ([self customBackgroundAttributesPresent]) {
+		[self drawCustomBackgroundForGlyphRange:glyphsToShow atPoint:origin];
+	} else {
+		[super drawBackgroundForGlyphRange:glyphsToShow atPoint:origin];
+	}
 }
 
+- (BOOL)customBackgroundAttributesPresent {
+	__block BOOL result = NO;
+	[self.textStorage  enumerateAttributesInRange:NSMakeRange(0, [self.textStorage length]) options:NSAttributedStringEnumerationLongestEffectiveRangeNotRequired usingBlock:
+	 ^(NSDictionary *attributes, NSRange range, BOOL *stop) {
+	 	if ([attributes.allKeys containsObject: RLHighlightedBackgroundCornerRadius]) {
+			result = YES;
+		}
+	 }];
+	return result;
+	}
+
+- (void)drawCustomBackgroundForGlyphRange:(NSRange)glyphsToShow atPoint:(CGPoint)origin {
+	NSRange range = glyphsToShow;
+	NSTextContainer *textContainer = [self textContainerForGlyphAtIndex:range.location
+														 effectiveRange:nil];
+	CGContextRef context = UIGraphicsGetCurrentContext();
+	CGContextSaveGState(context);
+	[self.textStorage enumerateAttribute:NSBackgroundColorAttributeName
+								 inRange:range
+								 options:NSAttributedStringEnumerationLongestEffectiveRangeNotRequired
+							  usingBlock:^(id value, NSRange range, BOOL *stop) {
+								  CGRect boundingRect = [self boundingRectForGlyphRange:range
+																		inTextContainer:textContainer];
+								  boundingRect.origin.x += origin.x;
+								  boundingRect.origin.y += origin.y;
+								  UIColor *backgroundColor = [self.textStorage attribute:NSBackgroundColorAttributeName
+																				 atIndex:range.location
+																		  effectiveRange:nil];
+								  CGFloat cornerRadius = ((NSNumber *)[self.textStorage attribute:RLHighlightedBackgroundCornerRadius
+																						  atIndex:range.location
+																				   effectiveRange:nil]).floatValue;
+								  UIBezierPath *bezierPath = [UIBezierPath bezierPathWithRoundedRect:boundingRect
+																						cornerRadius:cornerRadius];
+								  if (backgroundColor) {
+									  CGContextSetFillColorWithColor(context, backgroundColor.CGColor);
+									  [bezierPath fill];
+								  }
+							  }];
+	CGContextRestoreGState(context);
+}
 @end
